898363c1b911561b3e56cdd46d27e4c73e71f02c
Andry Prokopenko <isp.fline@gmail.com>
2019-04-26 10:57:17 +0300

    Initial Commit

---
+++ mySummary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  mySummary
+//
+//  Created by Андрей Прокопенко on 4/26/19.
+//  Copyright © 2019 Andry Pro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
